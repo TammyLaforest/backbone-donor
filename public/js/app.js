eaaa04f9a4ed5339616bb1cf54913f683d7ab9d9
TammyLaforest <TammyLaforest@gmail.com>
2019-08-05 15:51:10 -0400

    I seem to have fixed the input box glitch upon the closure of the modal

--- public/js/app.js
+++ public/js/app.js
@@ -52,40 +52,121 @@ app.DonorView = Backbone.View.extend({
             email: this.model.attributes.email,
             donorID: this.model.attributes.donorID,
         }
-        let myModal = new app.ModalView(data)
+        let myModal = new app.ModalView(this.model.attributes, this.model)
+    },
+    // edit: function () {
+    //     this.$('.edit-donor').hide()
+    //     this.$('.toggle-donor').hide()
+    //     this.$('.delete-donor').show()
+    //     this.$('.update-donor').show()
+    //     this.$('.cancel').show()
+
+    //     let firstName = this.$('.firstName').html()
+    //     let lastName = this.$('.lastName').html()
+    //     let email = this.$('.email').html()
+
+    //     this.$('.firstName').html(`<input type="text" class="form-control firstName-update" value=${firstName} >`)
+
+    //     this.$('.lastName').html(`<input type="text" class="form-control lastName-update" value=${lastName} >`)
+
+    //     this.$('.email').html(`<input type="text" class="form-control email-update" value=${email} >`)
+    // },
+    // update: function () {
+    //     let newName = this.$('.firstName-update').val().trim()
+    //     let oldName = this.model.attributes.firstName.trim()
+
+    //     if (newName == oldName) {
+    //         console.log("by golly, those match")
+    //         donorsView.render()
+    //     } else {
+    //         console.log("hear ye")
+    //     }
+    //     this.model.set('firstName', newName)
+    //     this.model.set('lastName', this.$('.lastName-update').val().trim())
+    //     this.model.set('email', this.$('.email-update').val().trim())
+
+    //     this.model.save(null, {
+    //         success: function (response) {
+    //             console.log(`Successfully UPDATED donor with _id: ${response.toJSON()._id} `)
+    //         },
+    //         error: function (err) {
+    //             console.log('Failed to update donor!')
+    //         }
+    //     })
+
+    // },
+    // cancel: function () {
+    //     donorsView.render()
+    // },
+    // delete: function () {
+    //     if (confirm("Are you sure you want to delete this donor?")) {
+    //         this.model.destroy({
+    //             success: function (response) {
+    //                 console.log(`Successfully DELETED donor with _id: ${response.toJSON()._id} `)
+    //             },
+    //             error: function (err) {
+    //                 console.log('Failed to delete donor!')
+    //             }
+    //         })
+    //     } else {
+    //         donorsView.render()
+    //     }
+    // },
+    render: function () {
+        this.$el.html(this.template(this.model.toJSON()))
+        return this
+    }
+})
+
+app.ModalView = Backbone.View.extend({
+    el: '#modal-template-div',
+    tagname: 'div',
+    template: _.template($('#modal-template').html()),
+    events: {
+        'click .edit-donor': 'edit',
+        'click .update-donor': 'update',
+        // 'click .cancel': 'cancel',
+        'click .delete-donor': 'delete',
+        'click .closeInputs': 'cancel',
+        'click .resetInputs': 'resetInputs',
+        'hidden.bs.modal #exampleModalLong': 'resetInputs'
+    },
+    initialize: function (data, model) {
+        console.log(data._id)
+        this.$el.html(this.template(data))
+        this.model = model
+        return this
     },
     edit: function () {
-
         this.$('.edit-donor').hide()
         this.$('.toggle-donor').hide()
         this.$('.delete-donor').show()
         this.$('.update-donor').show()
         this.$('.cancel').show()
 
-        let firstName = this.$('.firstName').html()
-        let lastName = this.$('.lastName').html()
-        let email = this.$('.email').html()
+        let firstName = this.model.attributes.firstName
+        let lastName = this.model.attributes.lastName
+        let email = this.model.attributes.email
+
+        // firstName = this.$('.firstName').html()
+        // lastName = this.$('.lastName').html()
+        // email = this.$('.email').html()
 
         this.$('.firstName').html(`<input type="text" class="form-control firstName-update" value=${firstName} >`)
 
         this.$('.lastName').html(`<input type="text" class="form-control lastName-update" value=${lastName} >`)
 
         this.$('.email').html(`<input type="text" class="form-control email-update" value=${email} >`)
-
     },
     update: function () {
-        let newName = this.$('.firstName-update').val().trim()
-        let oldName = this.model.attributes.firstName.trim()
 
-        if (newName == oldName) {
-            console.log("by golly, those match")
-            donorsView.render()
-        } else {
-            console.log("hear ye")
-        }
-        this.model.set('firstName', newName)
-        this.model.set('lastName', this.$('.lastName-update').val().trim())
-        this.model.set('email', this.$('.email-update').val().trim())
+        let firstName = this.$('.firstName-update').val()
+        let email = this.$('.email-update').val()
+        let lastName = this.$('.lastName-update').val()
+
+        this.model.set('firstName', firstName)
+        this.model.set('lastName', lastName)
+        this.model.set('email', email)
 
         this.model.save(null, {
             success: function (response) {
@@ -96,9 +177,32 @@ app.DonorView = Backbone.View.extend({
             }
         })
 
+        this.$('.email').html(email)
+        this.$('.firstName').html(firstName)
+        this.$('.lastName').html(lastName)
+
+        this.$('.edit-donor').show()
+        this.$('.toggle-donor').show()
+        this.$('.delete-donor').hide()
+        this.$('.update-donor').hide()
+        this.$('.cancel').hide()
+
     },
     cancel: function () {
-        donorsView.render()
+        this.$('.edit-donor').show()
+        this.$('.toggle-donor').show()
+        this.$('.delete-donor').hide()
+        this.$('.update-donor').hide()
+        this.$('.cancel').hide()
+
+        this.$('.email').html(this.model.attributes.email)
+        this.$('.firstName').html(this.model.attributes.firstName)
+        this.$('.lastName').html(this.model.attributes.lastName)
+    },
+    resetInputs: function () {
+        this.$('.email').html()
+        this.$('.firstName').html()
+        this.$('.lastName').html()
     },
     delete: function () {
         if (confirm("Are you sure you want to delete this donor?")) {
@@ -110,59 +214,12 @@ app.DonorView = Backbone.View.extend({
                     console.log('Failed to delete donor!')
                 }
             })
+
         } else {
             donorsView.render()
         }
-    },
-    render: function () {
-        this.$el.html(this.template(this.model.toJSON()))
-        return this
     }
 })
-
-
-app.TestView = Backbone.View.extend({
-    el: '#putsomethinghere',
-    tagname: 'div',
-    template: _.template($('#second-template').html()),
-    initialize: function () {
-        this.$el.html(this.template({ title: "this title" }))
-        return this
-    }
-})
-
-app.DonorContactView = Backbone.View.extend({
-    el: '#donor-contact-view',
-    tagname: 'div',
-    template: _.template($('#donor-contact-template').html()),
-    initialize: function () {
-        let data = {
-            firstName: "john",
-            lastName: "smith",
-            email: "johnsmith@example.com"
-        };
-        this.$el.html(this.template(data))
-        return this
-    }
-})
-
-app.ModalView = Backbone.View.extend({
-
-    el: '#modal-template-div',
-    tagname: 'div',
-    template: _.template($('#modal-template').html()),
-    initialize: function (data) {
-        // let data = {
-        //     firstName: "john",
-        //     lastName: "smith",
-        //     email: "johnsmith@example.com"
-        // }
-        this.$el.html(this.template(data))
-        return this
-    }
-})
-let testView = new app.TestView()
-let donorContactView = new app.DonorContactView()
 
 app.DonorsView = Backbone.View.extend({
     el: '#main',
@@ -186,6 +243,7 @@ app.DonorsView = Backbone.View.extend({
         let self = this
         this.$el.html('')
         _.each(this.model.toArray(), function (donor) {
+
             self.$el.append((new app.DonorView({ model: donor })).render().$el)
         })
         return this
@@ -218,3 +276,15 @@ $(document).ready(function () {
         })
     })
 })
+
+// app.TestView = Backbone.View.extend({
+//     el: '#putsomethinghere',
+//     tagname: 'div',
+//     template: _.template($('#second-template').html()),
+//     initialize: function () {
+//         this.$el.html(this.template({ title: "this title" }))
+//         return this
+//     }
+// })
+
+// let testView = new app.TestView()
